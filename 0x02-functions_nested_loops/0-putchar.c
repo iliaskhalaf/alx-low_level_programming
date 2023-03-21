7fd6fe8553406846492df42cb738129c32138660
ilias khalaf <iliaskhlf94@gmail.com>
2023-03-21 16:26:02 -0700

    putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,9 +1,6 @@
 #include <stdio.h>
-/* more headers goes there /
-/ betty style doc for function main goes there*/
 /**
  * main - prints all alphabet in lower case x 10.
- *
  * Return: Always 0 (Success)
  */
 int main(void)
