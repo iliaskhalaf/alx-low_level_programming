32232d13ed2f0566aea9c71aa9638e61a9e5a160
ilias khalaf <iliaskhlf94@gmail.com>
2023-03-21 17:32:14 -0700

    1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,12 +1,13 @@
 #include "main.h"
+
+
 /**
  * print_alphabet - prints all lowercase letters of the alphabet to the console
- *
  * Description:loop to iterate through each letter of
  * the alphabet in lowercase and prints them to the console
- *
  * Return: void
  */
+
 void print_alphabet(void)
 {
         char c = 'a';
