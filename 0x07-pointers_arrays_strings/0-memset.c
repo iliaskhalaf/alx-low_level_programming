392f623183197a7669ba188f120f370401cadedf
iliaskhalaf <iliaskhlf94@gmail.com>
2023-03-24 18:34:22 +0100

    aa

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -17,14 +17,3 @@ char *_memset(char *s, char b, unsigned int n)
 	}
 	return (s);
 }
-int main(void)
-{
-    char buffer[98] = {0};
-    char buffer2[98] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14};
-
-    simple_print_buffer(buffer, 98);
-    _memcpy(buffer + 50, buffer2, 10);
-    printf("-------------------------------------------------\n");
-    simple_print_buffer(buffer, 98);    
-    return (0);
-}
