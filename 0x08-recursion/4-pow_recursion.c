88a932d6ad81e3d2b464b9f86bd10d40c5903617
iliaskhalaf <iliaskhlf94@gmail.com>
2023-04-05 01:12:23 +0000

    powrecursion

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -1,29 +1,18 @@
 #include "main.h"
 
 /**
- * sqrt2 - Makes possible to evaluate from 1 to n
- * @a: same number as n
- * @b: number that iterates from 1 to n
+ * _pow_recursion - raises x to the power of y
+ * @x: Number Integer
+ * @y: Power
  *
  * Return: On success 1.
  * On error, -1 is returned, and errno is set appropriately.
  */
-int sqrt2(int a, int b)
+int _pow_recursion(int x, int y)
 {
-	if (b * b == a)
-		return (b);
-	else if (b * b > a)
+	if (y < 0)
 		return (-1);
-	return (sqrt2(a, b + 1));
-}
-/**
- * _sqrt_recursion - returns the natural square root of n
- * @n: Number Integer
- *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
- */
-int _sqrt_recursion(int n)
-{
-	return (sqrt2(n, 1));
+	if (y == 0)
+		return (1);
+	return (x * _pow_recursion(x, y - 1));
 }
